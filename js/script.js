260b64e7af4601c283b7b9f588f71c2329bb472e
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-04-04 12:27:26 -0700

    Completed most of the functionality of the calculator, array based calculation now lets you add, substract, multiply and divide muliple numbers while respecting the operator precedence

--- js/script.js
+++ js/script.js
@@ -2,40 +2,90 @@ const ans = document.querySelector('#ans');
 
 function add (value1, value2){return (value1+value2)}
 function substract (value1, value2){return (value1-value2)}
-function multiply (value1, value2){return (value1*value2)}
-function divide (value1, value2){return (value1/value2)}
+function times (value1, value2){return (value1*value2)}
+function between (value1, value2){return (value1/value2)}
 
 function operate (operator, value1, value2){
-    if (operator == "+") return add (value1, value2);
-    if (operator == "-") return substract (value1, value2);
-    if (operator == "*") return multiply (value1, value2);
-    if (operator == "/") return divide (value1, value2);
+    if (operator == "+") return add (parseFloat(value1), parseFloat(value2));
+    if (operator == "-") return substract (parseFloat(value1), parseFloat(value2));
+    if (operator == "*") return times (parseFloat(value1), parseFloat(value2));
+    if (operator == "/") return between (parseFloat(value1), parseFloat(value2));
 }
 
-display.innerHTML = operate("/",100,10);
-let firstNumber=["","","",""];
+//display.innerHTML = operate("+",100,100);
+display.innerHTML = "";
+let calcNumber = [];
+let calcOperator = []; 
 let n = 0;
 
 document.querySelectorAll(".calcBtn").forEach(function(button) {
     button.addEventListener("click", function(e){
-        var dot = firstNumber[n].includes(".");
-        if(dot == false || this.innerHTML != ".") firstNumber[n] += this.innerHTML;
-        display.innerHTML = firstNumber[n];
+        var dot = display.innerHTML.includes(".");
+        if(dot == false || this.innerHTML != ".") display.innerHTML += this.innerHTML;
+        //display.innerHTML = calcNumber[n];
     });
       });
 
     plus = document.querySelector ("#plusBtn");
     plus.addEventListener("click", function(e){
-        alert(firstNumber[n]);
+        calcNumber.push(display.innerHTML);
+        display.innerHTML = "";
+        calcOperator.push("+");
         n++;
     })
 
     minus = document.querySelector ("#minusBtn");
     minus.addEventListener("click", function(e){
-        var num1 = parseInt(firstNumber[0]);
-        var num2 = parseInt(firstNumber[1]);
-        alert(num1+num2);
-        
+        //var num1 = parseInt(calcNumber[0]);
+        calcNumber.push(display.innerHTML);
+        display.innerHTML = "";
+        calcOperator.push("-");
+        n++;
+    })
+
+    multiply = document.querySelector ("#multiplyBtn");
+    multiply.addEventListener("click", function(e){
+        calcNumber.push(display.innerHTML);
+        display.innerHTML = "";
+        calcOperator.push("*");
+        n++;
+    })
+
+    divide = document.querySelector ("#divideBtn");
+    divide.addEventListener("click", function(e){
+        console.log (operate("+",1,0.5));
+        calcNumber.push(display.innerHTML);
+        display.innerHTML = "";
+        calcOperator.push("/");
+        n++;
+    })
+
+    equal = document.querySelector ("#equalBtn");
+    equal.addEventListener("click", function(e){
+        calcNumber.push(display.innerHTML);
+        display.innerHTML = "";     
+        console.log(calcNumber);
+        console.log(calcOperator);
+        for (i = 0;i < calcOperator.length; i++){
+            while (calcOperator[i] == "*" || calcOperator[i] == "/"){
+                var result = operate(calcOperator[i],calcNumber[i],calcNumber[i+1]);
+                calcNumber.splice(i,2,result.toString());
+                calcOperator.splice(i,1);
+                
+            }
+        }
+        console.log(calcNumber);
+        console.log(calcOperator);
+        for (i = 0;i < calcOperator.length; i++){
+            while (calcOperator[i] == "+" || calcOperator[i] == "-"){
+                var result = operate(calcOperator[i],calcNumber[i],calcNumber[i+1]);
+                calcNumber.splice(i,2,result.toString());
+                calcOperator.splice(i,1);
+            }
+        }
+        display.innerHTML = calcNumber[0];
+        console.log(calcNumber);
+        console.log(calcOperator);
     })
 
 
